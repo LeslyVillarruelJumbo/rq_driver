28fe93be1a8bbcedd266ab319750eab7aa5c263b
Sebasss <sebas.cruz750@gmail.com>
2025-01-18 15:54:15 -0500

    chore: dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // APIs
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
 }

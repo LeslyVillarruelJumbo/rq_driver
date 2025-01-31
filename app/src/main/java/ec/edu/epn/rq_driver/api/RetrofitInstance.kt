c0f5d855a00a52c3486bd79bc4e1983af1c5fa3e
mplac555 <marcelo.placencia@epn.edu.ec>
2025-01-31 04:15:58 -0500

    add: Retrofit instance implementation

--- app/src/main/java/ec/edu/epn/rq_driver/api/RetrofitInstance.kt
+++ app/src/main/java/ec/edu/epn/rq_driver/api/RetrofitInstance.kt
@@ -1,3 +1,18 @@
 package ec.edu.epn.rq_driver.api
 
 object RetrofitInstance
+import ec.edu.epn.rq_driver.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+
+    private const val BASE_URL = "http://${BuildConfig.SERVER_URL}:${BuildConfig.SERVER_PORT}"
+    private val retrofitBuilder: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}

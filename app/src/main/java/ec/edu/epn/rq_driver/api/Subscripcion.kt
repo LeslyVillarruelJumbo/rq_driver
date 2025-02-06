5307b7672d152d76c7642ba17d5ebf25f7a07fca
Sebasss <sebas.cruz750@gmail.com>
2025-02-06 17:25:28 -0500

    add: methods for Subscripcion-api

--- app/src/main/java/ec/edu/epn/rq_driver/api/Subscripcion.kt
+++ app/src/main/java/ec/edu/epn/rq_driver/api/Subscripcion.kt
@@ -1,8 +1,10 @@
 package ec.edu.epn.rq_driver.api
 
+import ec.edu.epn.rq_driver.model.Usuario
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface Subscripcion {
 
@@ -11,4 +13,7 @@ interface Subscripcion {
 
   @GET("/api/subs")
   suspend fun getSubscriptions(): List<Subscripcion>
+
+  @GET("/api/subs/route/{routeId}")
+  suspend fun getUsersByRoute(@Path("routeId") routeId: String): List<Usuario>
 }
